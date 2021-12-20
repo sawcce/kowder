058e94605a04563c048f073784e2fc0aa1567af9
SauceArgente <alexis.collieux@gmail.com>
2021-12-20 11:23:00 +0100

    Added Startup page

--- Program.cs
+++ Program.cs
@@ -8,9 +8,15 @@ namespace kowder
     {
         static void Main(string[] args)
         {
-            Task.Run(() => Packages.Load());
+            Packages.Load();
 
             KowderEditor.Init();
+            Window.SetRenderMethod(delegate ()
+            {
+                Window.GetCanvas().DrawRect(0,0,10,10, new SKPaint{
+                    Color = new SKColor(235,125,10, 255)
+                });
+            });
             Window.Init();
         }
 
